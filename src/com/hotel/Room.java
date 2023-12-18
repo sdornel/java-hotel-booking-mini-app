6a6e098acee3067553e3e130c4057cb9eb985362
kaidornel <kai_dornel@hotmail.com>
2023-12-18 18:57:59 -0500

    readded room.java file

---
+++ src/com/hotel/Room.java
@@ -0,0 +1,33 @@
+package com.hotel;
+
+public class Room {
+    private int number;
+    private RoomType type;
+    
+    public enum RoomType {
+        ONEBED, TWOBED, SUITE
+    }
+
+    public Room(int number, RoomType type) {
+        this.number = number;
+        this.type = type;
+    }
+
+    // Getters
+    public int getNumber() {
+        return number;
+    }
+
+    public RoomType getType() {
+        return type;
+    }
+
+    // Setters
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setType(RoomType type) {
+        this.type = type;
+    }
+}
